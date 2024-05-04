72e44b0393d6ede1d61031d710d7a446cd4f6fea
Miquinon <qui18009@byui.edu>
2024-05-03 20:48:52 -0500

    added js

--- index.html/scripts/getdates.js
+++ index.html/scripts/getdates.js
@@ -1,1 +1,6 @@
-alert(document.lastModified);
+// lastModified
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last Modification: " + text;
+
+// Copyright
+document.getElementById("copyrightYear").innerHTML = new Date().getFullYear();
