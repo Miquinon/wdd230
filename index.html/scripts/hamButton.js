fa3a8c1e12af9073882c92e24536a04c41c8f2bf
Miquinon <qui18009@byui.edu>
2024-05-13 13:46:51 -0500

    worked on hambuger button

--- index.html/scripts/hamButton.js
+++ index.html/scripts/hamButton.js
@@ -1,7 +1,15 @@
-const hamButton = document.querySelector('#menu');
+const hamButton = document.querySelector('#hamButton');
 const navigation = document.querySelector('.navigation');
+
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
+	if (navigation.style.display=== "block") {
+		navigation.style.display="none";
+	}
+	else {
+		navigation.style.display="block";
+	}
 });
+
