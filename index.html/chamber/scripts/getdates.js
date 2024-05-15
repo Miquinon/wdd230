6161d0f74868d0d684cd540205582d9deb73b033
Miquinon <qui18009@byui.edu>
2024-05-14 21:47:58 -0500

    added chamber html

---
+++ index.html/chamber/scripts/getdates.js
@@ -0,0 +1,5 @@
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last Modification: " + text;
+
+// Copyright
+document.getElementById("copyrightYear").innerHTML = new Date().getFullYear();
