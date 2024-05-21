a71f57cddecb975952b546fa915b0c5dad942b17
Miquinon <qui18009@byui.edu>
2024-05-20 22:02:51 -0500

    fixed hamburger button

--- index.html/chamber/scripts/hamButton.js
+++ index.html/chamber/scripts/hamButton.js
@@ -5,4 +5,19 @@ const navigation = document.querySelector('.navigation');
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
+
+	if (navigation.style.display==="block") {
+		navigation.style.display="none";
+	}
+	else {
+		navigation.style.display="block";
+	}
+	function resetMenu(){
+		if(window.innerWidth >= 800){
+			document.querySelector('.navigation').style.display= 'flex';
+		}	else if (window.innerWidth < 800) {
+			document.querySelector('.navigation').style.display= 'none';
+		}
+	}
+	window.addEventListener('resize', resetMenu);
 });
