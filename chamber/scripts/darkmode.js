3479b8e53305159e1502661f86a6b476b684c7b9
Miquinon <qui18009@byui.edu>
2024-06-10 17:15:11 -0500

    finished the java script for the visits and dark-m

---
+++ chamber/scripts/darkmode.js
@@ -0,0 +1,39 @@
+const darkmode = document.querySelector(".dark-mode");
+const main = document.querySelector("main");
+let paragraphs = document.querySelectorAll("p");
+let h1s = document.querySelectorAll("h1");
+let h2s = document.querySelectorAll("h2");
+let about = document.querySelector(".about");
+
+
+darkmode.addEventListener("click", () => {
+    if (darkmode.textContent.includes("🌑")) {
+        main.style.background = "#000";
+        main.style.color = "#fff";
+        about.style.background ="#000";
+        paragraphs.forEach(paragraph => {
+            paragraph.style.color = "#fff";
+        });
+        h1s.forEach(h1 => {
+            h1.style.color = "#fff";
+        });
+        h2s.forEach(h2 => {
+            h2.style.color = "#fff";
+        });
+        darkmode.textContent = "🌕";
+    } else {
+        main.style.background = "white";
+        main.style.color = "#401201";
+        about.style.background ="#E3D1C4";
+        paragraphs.forEach(paragraph => {
+            paragraph.style.color = "#401201";
+        });
+        h1s.forEach(h1 => {
+            h1.style.color = "#401201";
+        });
+        h2s.forEach(h2 => {
+            h2.style.color = "#401201";
+        });
+        darkmode.textContent = "🌑";
+    }
+});
--- chamber/scripts/getvisits.js
+++ chamber/scripts/getvisits.js
@@ -1,17 +1,16 @@
-const visitsDisplay = document.querySelector(".visits");
-
-// 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-
-// 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+const visit = document.querySelector(".visits");
+let visitMessage= "Welcome! Let us know if you have any questions.";
+let today = new Date();
+let lastVisitString = window.localStorage.getItem("last-visit");
+if (lastVisitString != null){
+    let  lastVisitDate = new Date(lastVisitString);
+    let dateDifference = Math.floor((today.getTime() - lastVisitDate.getTime()) / (24 * 60 * 60 * 1000));
+    visitMessage = `Last visited ${dateDifference} days ago.`;
+	if (dateDifference < 1 ){
+		visitMessage = `Back so soon! Awesome!`;
+	} else {
+		visitMessage = `Last visited ${dateDifference} days ago.`;
+	}
 }
-
-// 4️⃣ increment the number of visits by one.
-numVisits++;
-
-// 5️⃣ store the new visit total into localStorage, key=numVisits-ls
-localStorage.setItem("numVisits-ls", numVisits);
+visit.textContent = visitMessage;
+window.localStorage.setItem("last-visit", today.toString());
