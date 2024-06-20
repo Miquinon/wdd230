384d84dc985787e1f5099b4c398a76941d1449ef
Miquinon <qui18009@byui.edu>
2024-06-19 22:22:49 -0500

    added JS to prophet assignment

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -5,7 +5,30 @@ const cards = document.querySelector('#cards');
 async function getProphetData(){
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets);
+   
+    displayProphets(data.prophets);
 }
 
 getProphetData();
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophets) => {
+        
+        let card= document.createElement('section');
+        let fullNmae= document.createElement('__');
+        let portrait= document.createElement('img');
+
+        fullName.textContent = `${prophet._____} ____________`; // fill in the blank
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.____} ______________`); // fill in the blank
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(_______); //fill in the blank
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
