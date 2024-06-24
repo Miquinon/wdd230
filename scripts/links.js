15ce95601179899e06cbd3245d6e02d1c1d1c239
Miquinon <qui18009@byui.edu>
2024-06-23 22:25:48 -0500

    added images to data, added json files created

---
+++ scripts/links.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const baseURL = "https://miquinon.github.io/wdd230/";
+const linksURL = "https://miquinon.github.io/wdd230/data/links.json";
+
+const learnActivities = document.getElementById('activity');
+
+const getLinks = async function() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    // displayLinks(data.lessons);
+    console.log(data);
+  }
+
+// const displayLinks = function(data) {
+//     for (let index = 0; index < data.length; index++) {
+//         let activityList = document.createElement('li');
+
+//         activityList.innerHTML = `${data[index].lesson}: `;
+//         data[index].links.forEach((link, i) => {
+//             let activityLink = document.createElement('a');
+//             activityLink.href = link.url;
+//             activityLink.title = link.title;
+//             activityLink.innerHTML = link.title;
+//             activityLink.target = '_blank';
+           
+
+//             activityList.appendChild(activityLink)
+//         });
+
+//         learnActivities.appendChild(activityList);
+//     }
+        
+// };
+
+  
+  getLinks();
