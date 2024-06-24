ee8dc9653a1befab062491d614b82600bdcfc28a
Miquinon <qui18009@byui.edu>
2024-06-24 14:16:50 -0500

    added js and fixed links css

--- scripts/links.js
+++ scripts/links.js
@@ -8,30 +8,31 @@ const learnActivities = document.getElementById('activity');
 const getLinks = async function() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    // displayLinks(data.lessons);
+    displayLinks(data.lessons);
     console.log(data);
   }
 
-// const displayLinks = function(data) {
-//     for (let index = 0; index < data.length; index++) {
-//         let activityList = document.createElement('li');
-
-//         activityList.innerHTML = `${data[index].lesson}: `;
-//         data[index].links.forEach((link, i) => {
-//             let activityLink = document.createElement('a');
-//             activityLink.href = link.url;
-//             activityLink.title = link.title;
-//             activityLink.innerHTML = link.title;
-//             activityLink.target = '_blank';
+const displayLinks = function(data) {
+    for (let index = 0; index < data.length; index++) {
+        let activityList = document.createElement('li');
+        activityList.style.display = "flex";
+        activityList.innerHTML = `${data[index].lesson}: `;
+        data[index].links.forEach((link, i) => {
+            let activityLink = document.createElement('a');
+            
+            activityLink.href = link.url;
+            activityLink.title = link.title;
+            activityLink.innerHTML = `${link.title} &nbsp &nbsp &nbsp |`;
+            activityLink.target = '_blank';
            
 
-//             activityList.appendChild(activityLink)
-//         });
+            activityList.appendChild(activityLink)
+        });
 
-//         learnActivities.appendChild(activityList);
-//     }
+        learnActivities.appendChild(activityList);
+    }
         
-// };
+};
 
   
   getLinks();
