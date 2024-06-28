143a3b2111557d321323ca6dceaec974b2fed7f3
Miquinon <qui18009@byui.edu>
2024-06-27 22:12:23 -0500

    corrected code for weather lesson

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -2,30 +2,30 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid={f3f6c3c27bb1d7aeac29f313cdd73695}&units=imperial';
+const apiKey = 'f3f6c3c27bb1d7aeac29f313cdd73695'; 
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=${apiKey}&units=imperial`;
 
 async function apiFetch() {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data); // testing only
-        // displayResults(data); // uncomment when ready
-      } else {
-          throw Error(await response.text());
-      }
-    } catch (error) {
-        console.log(error);
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+      throw new Error(await response.text());
     }
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
-  function displayResults(data) {
-    currentTemp.innerHTML = `${data._____}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${______}.___`;
-    let desc = data.weather[0].______;
-    weatherIcon.setAttribute('___', _____);
-    weatherIcon.setAttribute('___', _____);
-    captionDesc.textContent = `${desc}`;
-  }
+}
 
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`; 
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
 
-  apiFetch();
+apiFetch();
