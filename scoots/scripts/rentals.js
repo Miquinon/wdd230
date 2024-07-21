75aa32548512f5b640f70ebce9d3b18ba30e2a63
Miquinon <qui18009@byui.edu>
2024-07-21 12:44:54 -0500

    added the styles for rentals page

---
+++ scoots/scripts/rentals.js
@@ -0,0 +1,23 @@
+// Select the display container
+const display = document.querySelector("#option-container");
+
+// Fetch data from the JSON file
+fetch("data/option.json")  // Make sure this path is correct
+  .then((response) => response.json())
+  .then((data) => {
+    // Process each option in the data
+    data.forEach((option) => {
+      // Create a new section element for each option
+      const optionDiv = document.createElement("section");
+      optionDiv.innerHTML = `
+        <img src="${option.image}" alt="${option.name}" />
+        <h3>${option.name}</h3>
+        <p>${option['half-reserve']}</p>
+        <p>${option['half-walkin']}</p>
+        <p>${option['full-reserve']}</p>
+        <p>${option['full-walkin']}</p>
+      `;
+      // Append the new section element to the display container
+      display.appendChild(optionDiv);
+    });
+  })
